75b2232aa24c5a438e90412d328a4a90d5064464
Johannes Schindelin <johannes.schindelin@gmx.de>
2024-10-07 18:33:51 +0200

    path-walk API: avoid adding a root tree more than once
    When adding tree objects, we are very careful to avoid adding the same
    tree object more than once. There was one small gap in that logic,
    though: when adding a root tree object. Two refs can easily share the
    same root tree object, and we should still not add it more than once.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- path-walk.c
+++ path-walk.c
@@ -326,8 +326,10 @@ static int setup_pending_objects(struct path_walk_info *info,
 		struct object *obj = pending->item;
 
 		/* Commits will be picked up by revision walk. */
-		if (obj->type == OBJ_COMMIT)
+		if (obj->type == OBJ_COMMIT || obj->flags & SEEN)
 			continue;
+
+		obj->flags |= SEEN;
 
 		/* Navigate annotated tag object chains. */
 		while (obj->type == OBJ_TAG) {
