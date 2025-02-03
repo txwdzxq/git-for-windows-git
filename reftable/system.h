6510a03732e956a34a8f65c2fbe235cd4dd46545
Patrick Steinhardt <ps@pks.im>
2025-02-03 09:03:48 +0100

    reftable: decouple from Git codebase by pulling in "compat/posix.h"
    The reftable library includes "git-compat-util.h" in order to get a
    POSIX-like programming environment that papers over various differences
    between platforms. The header also brings with it a couple of helpers
    specific to the Git codebase though, and over time we have started to
    use these helpers in the reftable library, as well.
    
    This makes it very hard to use the reftable library as a standalone
    library without the rest of the Git codebase, so other libraries like
    e.g. libgit2 cannot easily use it. But now that we have removed all
    calls to Git-specific functionality and have split out "compat/posix.h"
    as a separate header we can address this.
    
    Stop including "git-compat-util.h" and instead include "compat/posix.h"
    to finalize the decoupling of the reftable library from the rest of the
    Git codebase. The only bits which remain specific to Git are "system.h"
    and "system.c", which projects will have to provide.
    
    Signed-off-by: Patrick Steinhardt <ps@pks.im>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- reftable/system.h
+++ reftable/system.h
@@ -11,7 +11,8 @@ https://developers.google.com/open-source/licenses/bsd
 
 /* This header glues the reftable library to the rest of Git */
 
-#include "git-compat-util.h"
+#include "../compat/posix.h"
+#include <zlib.h>
 
 /*
  * Return a random 32 bit integer. This function is expected to return
