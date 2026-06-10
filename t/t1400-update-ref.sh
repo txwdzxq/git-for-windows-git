e8f12e0e950a3735ccc2ed0bed2552ec1dcdd078
Junio C Hamano <gitster@pobox.com>
2026-06-10 14:39:08 -0700

    t1400: have fifo test clean after itself
    One test in this script creates a pair of FIFOs, "in" and "out",
    that are named so generically that later tests may be tempted to use
    them.  By the time those later tests run a command with its output
    redirected to the file (e.g., "git foobar >out"), however, nobody is
    reading from the lingering FIFO, and the test gets blocked forever.
    
    Clean them up when the test finishes.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t1400-update-ref.sh
+++ t/t1400-update-ref.sh
@@ -1610,6 +1610,7 @@ test_expect_success 'transaction cannot restart ongoing transaction' '
 '
 
 test_expect_success PIPE 'transaction flushes status updates' '
+	test_when_finished "rm -f in out" &&
 	mkfifo in out &&
 	(git update-ref --stdin <in >out &) &&
 
